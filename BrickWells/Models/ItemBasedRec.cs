e30c9432288209dffbce1fa99bd9935d05746779
ccristil <130681546+ccristil@users.noreply.github.com>
2024-04-10 16:46:40 -0600

    this only the index and the ItemBasedRec

---
+++ BrickWells/Models/ItemBasedRec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BrickWells.Models;
+
+public partial class ItemBasedRec
+{
+    // [ForeignKey("ProductId")]
+    [Key]
+    public int ProductId { get; set; }
+    // public Product? Product { get; set; } 
+    public string? Name { get; set; }
+
+    public string? FirstRec { get; set; }
+
+    public string? SecondRec { get; set; }
+
+    public string? ThirdRec { get; set; }
+
+    public string? FourthRec { get; set; }
+
+    public string? FifthRec { get; set; }
+
+    public string? SixthRec { get; set; }
+
+    public string? SeventhRec { get; set; }
+
+    public string? EighthRec { get; set; }
+
+    public string? NinthRec { get; set; }
+
+    public string? TenthRec { get; set; }
+}
