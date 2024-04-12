76e9a8a4a249fe0cde047c8d06bedabdffe4e2dd
AsanteLA <119529382+AsanteLA@users.noreply.github.com>
2024-04-12 00:59:57 -0600

    app.db update

--- BrickWells/Models/Product.cs
+++ BrickWells/Models/Product.cs
@@ -37,4 +37,5 @@ public partial class Product
     [Required(ErrorMessage = "Please enter the product's category")]
     public string Category { get; set; }
     public string? SubCategory { get; set; }
+    public int Rank { get; set; }
 }
