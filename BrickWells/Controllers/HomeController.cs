2c63a1d0f2b0a5e8b5666d4146c2ecbb7d04f629
ccristil <130681546+ccristil@users.noreply.github.com>
2024-04-11 21:29:07 -0600

    new commit

--- BrickWells/Controllers/HomeController.cs
+++ BrickWells/Controllers/HomeController.cs
@@ -167,64 +167,61 @@ public class HomeController : Controller
     // Check if the user is authenticated
     if (User.Identity.IsAuthenticated)
     {
-        // Retrieve the customer ID of the logged-in user
-        var customerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        // Retrieve the user's unique identifier (e.g., email address)
+        var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
 
-        if (customerId != null)
+        if (userEmail != null)
         {
-            // Convert the customer ID to the appropriate data type (e.g., int)
-            int parsedCustomerId;
-            if (int.TryParse(customerId, out parsedCustomerId))
-            {
-                // Look up customer ID in the customer table
-                var customerQuery = _repo.Customers.SingleOrDefault(x => x.CustomerId == parsedCustomerId);
+            // Query the Customers table to find the customerId associated with the logged-in user's email
+            var customer = _repo.Customers.FirstOrDefault(c => c.Email == userEmail);
 
-                // If the user is found, proceed to fetch their recent orders
-                if (customerQuery != null)
+            if (customer != null)
+            {
+                var customerId = customer.CustomerId;
+
+                // Proceed with retrieving the customer's recent orders and other information
+                var recentOrder = _repo.Orders
+                    .Where(x => x.CustomerId == customerId)
+                    .OrderByDescending(x => x.Date)
+                    .FirstOrDefault();
+
+                if (recentOrder != null)
                 {
-                    var recentOrder = _repo.Orders
-                        .Where(x => x.CustomerId == parsedCustomerId)
-                        .OrderByDescending(x => x.Date)
+                    // Retrieve the product ID from the recent order
+                    var transactionId = recentOrder.TransactionId;
+                    var productId = _repo.OrderDetails
+                        .Where(od => od.TransactionId == transactionId)
+                        .Select(od => od.ProductId)
                         .FirstOrDefault();
 
-                    if (recentOrder != null)
+                    if (productId != null)
                     {
-                        // Retrieve the product ID from the recent order
-                        var transactionId = recentOrder.TransactionId;
-                        var productId = _repo.OrderDetails
-                            .Where(od => od.TransactionId == transactionId)
-                            .Select(od => od.ProductId)
-                            .FirstOrDefault();
+                        // Retrieve the item-based recommendations based on the product ID
+                        var itemBasedRecs = _repo.ItemBasedRecs
+                            .SingleOrDefault(x => x.ProductId == productId);
 
-                        if (productId != null)
+                        if (itemBasedRecs != null)
                         {
-                            // Retrieve the item-based recommendations based on the product ID
-                            var itemBasedRecs = _repo.ItemBasedRecs
-                                .SingleOrDefault(x => x.ProductId == productId);
-
-                            if (itemBasedRecs != null)
+                            // Retrieve the IDs of the recommended products
+                            var recommendedProductIds = new List<string?>()
                             {
-                                // Retrieve the IDs of the recommended products
-                                var recommendedProductIds = new List<string?>()
-                                {
-                                    itemBasedRecs.FirstRec,
-                                    itemBasedRecs.SecondRec,
-                                    itemBasedRecs.ThirdRec,
-                                    itemBasedRecs.FourthRec,
-                                    itemBasedRecs.FifthRec
-                                };
+                                itemBasedRecs.FirstRec,
+                                itemBasedRecs.SecondRec,
+                                itemBasedRecs.ThirdRec,
+                                itemBasedRecs.FourthRec,
+                                itemBasedRecs.FifthRec
+                            };
 
-                                // Fetch additional details for the recommended products using the IDs
-                                var recommendedProducts = _repo.Products
-                                    .Where(x => recommendedProductIds.Contains(x.Name))
-                                    .ToList();
+                            // Fetch additional details for the recommended products using the IDs
+                            var recommendedProducts = _repo.Products
+                                .Where(x => recommendedProductIds.Contains(x.Name))
+                                .ToList();
 
-                                // Set ViewBag variables for the view
-                                ViewBag.RecommendedProducts = recommendedProducts;
-                                ViewBag.TransactionId = transactionId;
-                                ViewBag.Customer = customerQuery;
-                                ViewBag.Orders = recentOrder;
-                            }
+                            // Set ViewBag variables for the view
+                            ViewBag.RecommendedProducts = recommendedProducts;
+                            ViewBag.TransactionId = transactionId;
+                            ViewBag.Customer = customer;
+                            ViewBag.Orders = recentOrder;
                         }
                     }
                 }
@@ -237,11 +234,89 @@ public class HomeController : Controller
     {
         Products = _repo.Products.OrderBy(x => x.Rank).Take(5)
     };
-    
-    
-    
 
     return View(brick);
 }
+
+    
+//     public IActionResult Index()
+// {
+//     // Check if the user is authenticated
+//     if (User.Identity.IsAuthenticated)
+//     {
+//         // Retrieve the customer ID of the logged-in user
+//         var customerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//         // var customerId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//
+//         if (customerId != null)
+//         {
+//             // Convert the customer ID to the appropriate data type (e.g., int)
+//             int parsedCustomerId;
+//             if (int.TryParse(customerId, out parsedCustomerId))
+//             {
+//                 // Look up customer ID in the customer table
+//                 var customerQuery = _repo.Customers.SingleOrDefault(x => x.CustomerId == parsedCustomerId);
+//
+//                 // If the user is found, proceed to fetch their recent orders
+//                 if (customerQuery != null)
+//                 {
+//                     var recentOrder = _repo.Orders
+//                         .Where(x => x.CustomerId == parsedCustomerId)
+//                         .OrderByDescending(x => x.Date)
+//                         .FirstOrDefault();
+//
+//                     if (recentOrder != null)
+//                     {
+//                         // Retrieve the product ID from the recent order
+//                         var transactionId = recentOrder.TransactionId;
+//                         var productId = _repo.OrderDetails
+//                             .Where(od => od.TransactionId == transactionId)
+//                             .Select(od => od.ProductId)
+//                             .FirstOrDefault();
+//
+//                         if (productId != null)
+//                         {
+//                             // Retrieve the item-based recommendations based on the product ID
+//                             var itemBasedRecs = _repo.ItemBasedRecs
+//                                 .SingleOrDefault(x => x.ProductId == productId);
+//
+//                             if (itemBasedRecs != null)
+//                             {
+//                                 // Retrieve the IDs of the recommended products
+//                                 var recommendedProductIds = new List<string?>()
+//                                 {
+//                                     itemBasedRecs.FirstRec,
+//                                     itemBasedRecs.SecondRec,
+//                                     itemBasedRecs.ThirdRec,
+//                                     itemBasedRecs.FourthRec,
+//                                     itemBasedRecs.FifthRec
+//                                 };
+//
+//                                 // Fetch additional details for the recommended products using the IDs
+//                                 var recommendedProducts = _repo.Products
+//                                     .Where(x => recommendedProductIds.Contains(x.Name))
+//                                     .ToList();
+//
+//                                 // Set ViewBag variables for the view
+//                                 ViewBag.RecommendedProducts = recommendedProducts;
+//                                 ViewBag.TransactionId = transactionId;
+//                                 ViewBag.Customer = customerQuery;
+//                                 ViewBag.Orders = recentOrder;
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//
+//     // If the user is not authenticated or encountered any errors, proceed with default suggestions
+//     var brick = new BrickListViewModel
+//     {
+//         Products = _repo.Products.OrderBy(x => x.Rank).Take(5)
+//     };
+//     
+//     return View(brick);
+// }
 
 }
